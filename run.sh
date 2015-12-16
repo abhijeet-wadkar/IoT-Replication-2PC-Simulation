71a12ccfd023fa1fb0817141f851a15ff4e6cb34
abhijeet-wadkar <arwadkar@gmail.com>
2015-12-15 22:43:36 -0500

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-cd obj/gateway_front_tier/
-gnome-terminal -e "./gateway_front_tier ../../ConfigFiles/PrimaryGatewayConfig.txt ../../output/PrimaryGateway.log"
-gnome-terminal -e "./gateway_front_tier ../../ConfigFiles/SecondaryGatewayConfig.txt ../../output/SecondaryGateway.log"
-cd ../gateway_back_tier/
-gnome-terminal -e "./gateway_back_tier ../../ConfigFiles/PrimaryBackendConfig.txt ../../output/PrimaryBackend.log"
-gnome-terminal -e "./gateway_back_tier ../../ConfigFiles/SecondaryBackendConfig.txt ../../output/SecondaryBackend.log"
-cd ../door_sensor/
-gnome-terminal -e "./door_sensor  ../../ConfigFiles/DoorConfig.txt ../../InputFiles/DoorInput.txt ../../output/Door.log"
-cd ../motion_sensor/
-gnome-terminal -e "./motion_sensor ../../ConfigFiles/MotionConfig.txt ../../InputFiles/MotionInput.txt ../../output/Motion.log"
-cd ../key_chain_sensor/
-gnome-terminal -e "./key_chain_sensor ../../ConfigFiles/KeychainConfig.txt ../../InputFiles/KeychainInput.txt ../../output/Keychain.log"
-cd ../device/
-gnome-terminal -e "./device ../../ConfigFiles/SecurityDeviceConfig.txt ../../output/Device.log"
-cd ../../
+gnome-terminal -e "bash -c \"./output/gateway_front_tier ConfigFiles/PrimaryGatewayConfig.txt PrimaryGatewayOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/gateway_front_tier ConfigFiles/SecondaryGatewayConfig.txt SecondaryGatewayOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/gateway_back_tier ConfigFiles/PrimaryBackendConfig.txt PrimaryBackendOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/gateway_back_tier ConfigFiles/SecondaryBackendConfig.txt SecondaryBackendOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/motion_sensor ConfigFiles/MotionConfig.txt InputFiles/MotionInput.txt MotionOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/device ConfigFiles/SecurityDeviceConfig.txt SecurityDeviceOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/door_sensor ConfigFiles/DoorConfig.txt InputFiles/DoorInput.txt DoorOutput.log; exec bash\""
+sleep 4
+gnome-terminal -e "bash -c \"./output/key_chain_sensor ConfigFiles/KeychainConfig.txt InputFiles/KeychainInput.txt KeychainOutput.log; exec bash\""
