824c8f81663153fc892ae0c71471c0aad9fe1522
abhijeet-wadkar <arwadkar@gmail.com>
2015-12-15 22:44:13 -0500

    changed makefiles

--- make/base_make.mk
+++ make/base_make.mk
@@ -5,12 +5,11 @@ CCINCLUDES := $(addprefix -I, $(INCLUDE_PATH))
 OBJS := $(SRCS:.c=.o)
 OBJS_FULL := $(addprefix $(OBJ_DIR)/, $(OBJS))
 
-all : $(TOP_DIR)/obj/$(EXE_NAME)
+all : $(TOP_DIR)/output/$(EXE_NAME)
 
-$(TOP_DIR)/obj/$(EXE_NAME) : $(OBJS_FULL)
+$(TOP_DIR)/output/$(EXE_NAME) : $(OBJS_FULL)
 	@echo "Making EXE"
-	@mkdir -p $(TOP_DIR)/obj/$(EXE_NAME)/
-	@$(CC) -o $(TOP_DIR)/obj/$(EXE_NAME)/$(EXE_NAME) $(OBJS_FULL) $(EXT_LIB)
+	@$(CC) -o $(TOP_DIR)/output/$(EXE_NAME) $(OBJS_FULL) $(EXT_LIB)
 
 $(OBJ_DIR)/%.o: $(TOP_DIR)/%.c
 	@echo "Compiling "$<
